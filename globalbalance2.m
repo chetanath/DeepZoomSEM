16ac9cd4780a9d38acd82e2e650c07bf2165874d
ogliore <43612703+ogliore@users.noreply.github.com>
2019-11-04 16:16:41 -0600

    Add files via upload

---
+++ globalbalance2.m
@@ -0,0 +1,174 @@
+function [XX_factor,OFFSET]=globalbalance2(brightnessvalues,brightnessvaluesv,cols_array,colt_split)
+% function [XX_factor,OFFSET]=globalbalance2(brightnessvalues,brightnessvaluesv,cols_array,colt_split)
+
+threshdiff=1e-5;
+
+
+sz1=size(brightnessvalues,1);
+sz2=size(brightnessvalues,2);
+
+XX_offset=nan(sz1,sz2);
+OFFSET=nan(sz1,sz2);
+XX_factor=nan(sz1,sz2);
+
+stdimg_=inf;
+stdimg=realmax;
+
+iternumber=0;
+
+meanbrightness0=nanmean(brightnessvalues(:));
+
+while stdimg/stdimg_<(1-threshdiff) %diffsumsq./diffsumsqp<0.95
+
+    iternumber=iternumber+1;
+    
+    stdimg_=stdimg;
+        
+    for qq=cols_array
+        
+        if qq<=colt_split
+            qqn=qq-1;
+        else
+            qqn=qq+1;
+        end
+        
+        firstpair=find(isfinite(brightnessvalues(qq,:,1)),1,'first');
+        lastpair=find(isfinite(brightnessvalues(qq,:,1)),1,'last');
+        
+        
+        for kk=firstpair+1:lastpair
+            
+            allhist1=nan(4,1);
+            allhist2=nan(4,1);
+            
+            
+            if sum(isnan(brightnessvalues(qq,kk,:)))==0
+                allhist1(1)=brightnessvalues(qq,kk,1);
+                allhist2(1)=brightnessvalues(qq,kk,2);
+                
+            end
+            
+            if sum(isnan(brightnessvaluesv(qq,kk,:)))==0
+                allhist1(2)=brightnessvaluesv(qq,kk,1);
+                allhist2(2)=brightnessvaluesv(qq,kk,2);
+                
+            end
+            
+            
+            if kk+1<sz2
+                if sum(isnan(brightnessvalues(qq,kk+1,:)))==0
+                    allhist1(3)=brightnessvalues(qq,kk+1,2);
+                    allhist2(3)=brightnessvalues(qq,kk+1,1);
+                end
+            end
+            
+            if qqn<=max(cols_array) && qqn>=min(cols_array)
+                if sum(isnan(brightnessvaluesv(qqn,kk,:)))==0
+                    allhist1(4)=brightnessvaluesv(qqn,kk,2);
+                    allhist2(4)=brightnessvaluesv(qqn,kk,1);
+                    
+                end
+            end
+            
+            isnanallhist1=isnan(allhist1);
+            
+            if sum(~isnanallhist1)>0
+
+                isfinitehist=isfinite(allhist2) & isfinite(allhist1);
+                XX=rms(allhist2(isfinitehist))-rms(allhist1(isfinitehist));
+ 
+                if ~isnanallhist1(1)
+                    brightnessvalues(qq,kk,1)=brightnessvalues(qq,kk,1)+XX;
+                end
+                
+                if ~isnanallhist1(2)
+                    brightnessvaluesv(qq,kk,1)=brightnessvaluesv(qq,kk,1)+XX;
+                end
+                
+                if ~isnanallhist1(3)
+                    brightnessvalues(qq,kk+1,2)=brightnessvalues(qq,kk+1,2)+XX;
+                end
+                
+                if ~isnanallhist1(4)
+                    brightnessvaluesv(qqn,kk,2)=brightnessvaluesv(qqn,kk,2)+XX;
+                end
+                
+                if isnan(XX_offset(qq,kk))
+                    XX_offset(qq,kk)=XX;
+                else
+                    XX_offset(qq,kk)=XX_offset(qq,kk)+XX;
+                end
+
+            end
+            
+        end
+        
+        
+    end
+
+    stdimg=nanstd(brightnessvalues(:));                              
+    
+    fprintf('Iter=%d, New=%.6e, Old=%.6e, Ratio=%.3e\n',iternumber,stdimg,stdimg_,1-stdimg./stdimg_)
+    
+    
+    % meanpixelvalue=[meanpixelvalue,mean([nanmean(nanmean(brightnessvalues(:,:,1))), nanmean(nanmean(brightnessvaluesv(:,:,1)))])];
+       
+end
+
+% brightnessvalues=brightnessvalues.*meanbrightness0./nanmean(brightnessvalues(:));
+
+[X,Y]=ndgrid(1:size(brightnessvalues,1),1:size(brightnessvalues,2));
+BV=0.5.*(brightnessvalues(:,:,1)+brightnessvaluesv(:,:,1));
+isf=isfinite(BV(:));
+sf=fit([X(isf),Y(isf)],BV(isf),'poly33');
+
+if numel(cols_array)>1
+    
+    avgbrightness=nanmean(BV(:));
+    
+    for ii=1:size(brightnessvalues,1)
+        for jj=1:size(brightnessvalues,2)
+            shift=sf(X(ii),Y(ii));
+            if isfinite(shift)
+                OFFSET(ii,jj)=avgbrightness-shift;
+            end
+        end
+    end
+    
+end
+
+isfXX=isfinite(OFFSET);
+
+brightnessvalues(isfXX)=brightnessvalues(isfXX)+OFFSET(isfXX);
+brightnessvaluesv(isfXX)=brightnessvaluesv(isfXX)+OFFSET(isfXX);
+
+OFFSET=inpaint_nans(OFFSET+XX_offset,0);
+
+% brightnessvalues=brightnessvalues.*meanbrightness0./nanmean(brightnessvalues(:));
+
+[X,Y]=ndgrid(1:size(brightnessvalues,1),1:size(brightnessvalues,2));
+BV=0.5.*(brightnessvalues(:,:,1)+brightnessvaluesv(:,:,1));
+isf=isfinite(BV(:));
+sf=fit([X(isf),Y(isf)],BV(isf),'poly33');
+
+if numel(cols_array)>1
+    
+    avgbrightness=nanmean(BV(:));
+    
+    for ii=1:size(brightnessvalues,1)
+        for jj=1:size(brightnessvalues,2)
+            shift=sf(X(ii),Y(ii));
+            if isfinite(shift) && shift~=0
+                XX_factor(ii,jj)=avgbrightness./shift;
+            end
+        end
+    end
+    
+end
+
+XX_factor-inpaint_nans(XX_factor,0);
+
+%%
+
+
+
